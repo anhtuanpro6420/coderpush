ca5095edd646a8fa58d610ddce47f4759b30c543
si <anhtuanpro6420@gmail.com>
2022-01-21 22:02:37 +0700

    style of user card

--- components/UserCard/index.tsx
+++ components/UserCard/index.tsx
@@ -1,9 +1,8 @@
 import { Card } from 'antd';
 import React, { FC } from 'react';
 import Meta from 'antd/lib/card/Meta';
-import { CloseOutlined, HeartOutlined } from '@ant-design/icons';
 import { IUser } from '../../types/user.interface';
-import style from './UserCard.module.css';
+import styles from './UserCard.module.css';
 
 interface Props {
   user: IUser;
@@ -12,15 +11,14 @@ interface Props {
 const UserCard: FC<Props> = ({ user }) => {
   return (
     <Card
-      className="user-card"
+      className={styles.userCard}
       cover={
         <img
           src={user.picture}
           alt="User picture"
-          className={style.userPicture}
+          className={styles.userPicture}
         />
       }
-      actions={[<CloseOutlined key="ignore" />, <HeartOutlined key="like" />]}
     >
       <Meta title={user.firstName} />
     </Card>
