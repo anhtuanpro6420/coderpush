9581b050f1bccf45a052ffe9718d1b462bf4bf4b
si <anhtuanpro6420@gmail.com>
2022-01-24 23:01:35 +0700

    add mongodb and connection

---
+++ db/connection.db.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb';
+
+const connectDb = async () => {
+  try {
+    const url =
+      'mongodb+srv://si:Si%40210294@cluster0.wcid1.mongodb.net/coderpush?retryWrites=true&w=majority';
+    const client = new MongoClient(url);
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db('coderpush');
+    return db;
+  } catch (error) {
+    console.log('Can not connect to db: ', error);
+    throw error;
+  }
+};
+
+export default connectDb;
